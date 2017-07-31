9beaac932ecb07c038c2a57eca61fdc36e940a5f
James Byars <jamesbyars@g.harvard.edu>
2017-07-31 16:11:54 -0400

    Generate test report

---
+++ src/test/java/com/ernesttech/example/dbtdd/runner/BsTest.java
@@ -0,0 +1,14 @@
+package com.ernesttech.example.dbtdd.runner;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BsTest {
+
+    @Test
+    public void isTrueTrue() {
+        Assert.assertTrue(true);
+    }
+}
